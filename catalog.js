439a0722108fd5f2af95cf06880ba977fa338d0e
Show-K Adot <1143158498x@gmail.com>
2021-11-22 15:47:44 +0800

    Update 在线更新

--- catalog.js
+++ catalog.js
@@ -1,9 +1,9 @@
 extension["在线更新"]={
     author:"诗笺、Show-K（修改）",
-    intro:"点击按钮即可在线更新，文件下载失败会自动重新下载。</br>点击按钮后若短时间内没反应可以重启游戏。</br><span style='color:red'>请不要在更新时关闭游戏，否则后果自负</br>（更新时关闭游戏后，windows用户会有不能删除的残留文件，可以通过注销用户来自动清除）</span>",
-    size:"23.8KB",
+    intro:"点击按钮即可在线更新，文件下载失败会自动重新下载。</br><span style='color:red'>请不要在更新时关闭游戏，否则后果自负</span></br>最新完整包下载地址：<a target='_self' href='https://hub.fastgit.org/Show-K/noname/archive/refs/heads/super_smash_tabletop.zip'><span style='text-decoration: underline;'>点击下载</span></a>",
+    size:"23.6KB",
     netdisk:"",
-    version:1.18,
+    version:1.2,
     files:[
         "extension.js",
 	],
